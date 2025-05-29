d5071b555ce86bcdf6764d33c5f1acb922a3d9c6
= <=>
2025-05-29 14:43:46 +0900

    # 안내문구 수정2

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="welcome-message" v-if="isLoggedIn">
       <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
-      <p class="description">먀먀먀먀먀먀먀먀먀먀먀먀먀먀먀먀먀먀먐</p>
+      <p class="description">묘묘묘묘묘묘묘묘묘묘묘묘묭묭</p>
     </div>
     <div class="brand" v-if="isLoggedIn">
       LEAFY
